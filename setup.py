6c9b90bc0fcba21b0cb44d669e1a79dd85ea67f9
shikher <sirshikher@gmail.com>
2020-04-08 01:31:50 +0530

    intial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First Data Science Project based on Titanic DataSet',
+    author='Shikher',
+    license='MIT',
+)
